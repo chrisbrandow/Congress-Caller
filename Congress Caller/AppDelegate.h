96c056673c93297d1880901c75d66b898192e672
chrisbrandow <chrisbrandow@gmail.com>
2016-11-18 15:57:20 -0800

    Initial Commit

---
+++ Congress Caller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Congress Caller
+//
+//  Created by Christopher Brandow on 11/18/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Congress Caller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Congress Caller
+//
+//  Created by Christopher Brandow on 11/18/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
